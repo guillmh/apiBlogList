decbd7b6bc80c89fb4f761c064a902d0d47a599a
Guillber Mendez <guillbermndz@gmail.com>
2025-07-17 20:31:21 -0600

    Refactor: different environments are added in the package.json

--- utils/config.js
+++ utils/config.js
@@ -4,7 +4,10 @@ require("dotenv").config();
 // Extrae el puerto del servidor de las variables de entorno.
 const PORT = process.env.PORT;
 // Extrae la URI de conexión de MongoDB de las variables de entorno.
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
 
 // Exporta las variables para que puedan ser utilizadas en otras partes de la aplicación.
 module.exports = { MONGODB_URI, PORT };
