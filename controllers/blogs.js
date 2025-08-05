71d2fc9475f2cf146b421e6335e3334190116979
Guillber Mendez <guillbermndz@gmail.com>
2025-08-04 22:59:09 -0600

    feat: You can now get a single resource by id and update it

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -5,12 +5,26 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
 // Obtener todos los blogs
-blogsRouter.get("/", async (requ, res, next) => {
+blogsRouter.get("/", async (req, res, next) => {
   // Utiliza el método `find` del modelo Blog para buscar todos los documentos en la colección.
   // Un objeto de filtro vacío `{}` significa 'traer todos los documentos'.
   // El método `find` devuelve una promesa.
   const blogs = await Blog.find({});
   res.json(blogs);
+});
+
+//obtner un solo recurso por ID
+blogsRouter.get("/:id", async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const blog = await Blog.findById(id);
+    if (!blog) {
+      return res.status(404).json({ error: "Blog not found" });
+    }
+    res.json(blog);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // Crear un nuevo blog
@@ -39,6 +53,22 @@ blogsRouter.delete("/:id", async (req, res, next) => {
 });
 
 //Actualiza un recurso por ID
+blogsRouter.patch("/:id", async (req, res, next) => {
+  const id = req.params.id;
+  const dataBody = req.body;
+  try {
+    const update = await Blog.findByIdAndUpdate(id, dataBody, {
+      new: true,
+      runValidators: true,
+    });
+    if (!update) {
+      return res.status(404).json({ error: "blog not found" });
+    }
+    res.json(update);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Exportar enrutador
 module.exports = blogsRouter;
