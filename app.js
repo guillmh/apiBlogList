3ee8d279ec63f553f54dd444d419c8bcfc754879
Guillber Mendez <guillbermndz@gmail.com>
2025-09-08 22:00:36 -0600

    Feat: a user can authenticate

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
-const blogsRouter = require("./controllers/blogs");
 const middleware = require("./utils/middleware");
+
+const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 // Mongoose 7 cambiará el valor por defecto de strictQuery a true.
 // Se establece en false para prepararse para este cambio y suprimir la advertencia.
@@ -35,6 +37,7 @@ app.use(express.json());
 // Define el enrutador para las rutas que comienzan con /api/blogs.
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 // Middleware para manejar endpoints desconocidos (rutas no encontradas).
 // Se ejecuta si ninguna ruta anterior ha manejado la petición.
